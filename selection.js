9378626edcd13254b6fbf6a8d29564fed0b4434a
Arthur Hauser <arthur.hauser@gmail.com>
2024-09-23 14:19:33 -0400

    first and last commit

---
+++ selection.js
@@ -0,0 +1,21 @@
+// selection.js
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            // Swap
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+    return arr;
+}
+
+module.exports = selectionSort;
